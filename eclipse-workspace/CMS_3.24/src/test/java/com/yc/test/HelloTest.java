9be0a6cb8e5d38ea20fb12bbba7f882e609bd62e
YaTes <522211007@qq.com>
2020-03-25 09:05:09 +0800

    忽略提交

---
+++ eclipse-workspace/CMS_3.24/src/test/java/com/yc/test/HelloTest.java
@@ -0,0 +1,14 @@
+package com.yc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void testMain() {
+		System.out.println("hello");
+	}
+
+}
